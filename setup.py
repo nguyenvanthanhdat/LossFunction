2f8460132182919713bc9d068d2695e1a748d2bf
datnvt <nguyenvanthanhdat1810@gmail.com>
2023-11-05 15:20:17 +0700

    Feature: update requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import sys
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+# with open('LICENSE', encoding='utf-8') as f:
+#     license = f.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    reqs = f.read()
+
+setup(
+    name = 'loss_nli',
+    version='0.1.0',
+    description='Empirical study of loss functions for inference methods based on language models',
+    long_description='readme',
+    python_requires='>=3.11',
+    packages=find_packages(exclude=('data')),
+    install_requires=reqs.strip().split('\n'),
+)
