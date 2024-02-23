44d3f1bca015f6c14544b7c18e30d99185cf95b2
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-02-23 16:33:02 +0700

    Feat: add script training

---
+++ scripts/vinli_3_label_xlmr.sh
@@ -0,0 +1,32 @@
+MODEL_NAME_OR_PATH='xlm-roberta-large'
+DATASET_NAME='presencesw/vinli_3_label'
+DATASET='vinli_3_label'
+OUTPUT_DIR='output/'xlm-roberta-large'/vinli_3_label'
+RUN_NAME="'xlm-roberta-large'--vinli_3_label"
+BZ=14
+GRA_ACC=8
+
+!accelerate launch --num_processes 2 --gpu_ids 0,1 --config_file ds.yaml -m loss_nli.training \
+    --num_labels 3 \
+    --model_name_or_path $MODEL_NAME_OR_PATH \
+    --dataset_name $DATASET_NAME \
+    --output_dir $OUTPUT_DIR \
+    --do_train \
+    --train_dir 'data/finetune/train' \
+    --do_eval \
+    --valid_dir 'data/finetune/validation' \
+    --per_device_train_batch_size $BZ \
+    --per_device_train_batch_size $BZ \
+    --gradient_accumulation_steps $GRA_ACC \
+    --num_train_epochs 15 \
+    --save_steps 100 \
+    --eval_steps 100 \
+    --logging_steps 20 \
+    --evaluation_strategy 'steps' \
+    --overwrite_output_dir \
+    --loss_func_name 'cross' \
+    --load_best_model_at_end \
+    --save_total_limit 1 \
+    --overwrite_output_dir \
+    --run_name $RUN_NAME \
+    --save_only_model
