109c8226b9d85191b6b63a920ccaf23e724d37c4
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-02-24 15:03:26 +0700

    Feat: Just tokenizer in train split

--- src/loss_nli/data/data.py
+++ src/loss_nli/data/data.py
@@ -32,7 +32,7 @@ contract_label_dict = {
 def Find_max_length(dataset, split_dict, tokenize_name):
     # tokenizer = AutoTokenizer.from_pretrained(tokenizer_dict[tokenize_name])
     tokenizer = AutoTokenizer.from_pretrained(tokenize_name)
-    dataset = dataset.map(lambda examples: tokenizer(
+    dataset['train'] = dataset['train'].map(lambda examples: tokenizer(
         examples["sentence2"], 
         examples["sentence1"],
         ), batched=True)
