342f6eb28f1e1f3a51215bb854a6dad660b9e294
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-03-05 21:23:11 +0700

    Feat: Config run vinli 3 label cross xlmr

--- scripts/vinli_3_label_xlmr.sh
+++ scripts/vinli_3_label_xlmr.sh
@@ -1,13 +1,15 @@
 MODEL_NAME_OR_PATH='xlm-roberta-large'
-DATASET_NAME='presencesw/vinli_3_label'
-DATASET='vinli_3_label'
-OUTPUT_DIR='output/'xlm-roberta-large'/vinli_3_label'
-RUN_NAME="xlm-roberta-large--vinli_3_label"
+DATASET_NAME='presencesw/vinli_4_label'
+DATASET = DATASET_NAME.split("/")[1]
+OUTPUT_DIR=f'output/{MODEL_NAME_OR_PATH}/{DATASET}'
+RUN_NAME=f"{MODEL_NAME_OR_PATH}--{DATASET}_TEST"
+HF_TOKEN = userdata.get('hf_token')
 BZ=14
 GRA_ACC=8
 
-!accelerate launch --num_processes 2 --gpu_ids 0,1 -m loss_nli.training \
-    --num_labels 3 \
+!accelerate launch --num_processes 1 --gpu_ids 0 --config_file ds.yaml -m loss_nli.training \
+    --hf_token $HF_TOKEN \
+    --num_labels 4 \
     --model_name_or_path $MODEL_NAME_OR_PATH \
     --dataset_name $DATASET_NAME \
     --output_dir $OUTPUT_DIR \
@@ -19,17 +21,14 @@ GRA_ACC=8
     --per_device_train_batch_size $BZ \
     --gradient_accumulation_steps $GRA_ACC \
     --num_train_epochs 15 \
-    --evaluation_strategy 'epoch' \
-    --save_strategy 'epoch' \
-    --logging_strategy 'epoch' \
+    --save_steps 100 \
+    --eval_steps 100 \
+    --logging_steps 20 \
+    --evaluation_strategy 'steps' \
     --overwrite_output_dir \
     --loss_func_name 'cross' \
     --load_best_model_at_end \
     --save_total_limit 1 \
     --overwrite_output_dir \
     --run_name $RUN_NAME \
-    --save_only_model \
-    --lr_scheduler_type "linear" \
-    --learning_rate "5e-5"
-
-# 2 T4
+    --save_only_model
