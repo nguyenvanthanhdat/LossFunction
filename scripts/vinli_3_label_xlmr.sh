e639b40c922987cf7aa39f097d481fb1485e323d
HgThinker <102933694+HgThinker@users.noreply.github.com>
2024-03-10 11:15:36 +0700

    Update vinli_3_label_xlmr.sh
    correct comand

--- scripts/vinli_3_label_xlmr.sh
+++ scripts/vinli_3_label_xlmr.sh
@@ -8,7 +8,7 @@ HF_TOKEN='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'
 BZ=10
 GRA_ACC=8
 
-accelerate launch --num_processes 2 --gpu_ids 0, 1 --config_file ds.yaml -m loss_nli.training \
+accelerate launch --num_processes 2 --gpu_ids 0,1 --config_file ds.yaml -m loss_nli.training \
     --hf_token $HF_TOKEN \
     --wandb_project $WANDB_PROJECT \
     --num_labels 3 \
